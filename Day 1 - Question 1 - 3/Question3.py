329814aa3125deaa76207d24fd1c37309ed99ffd
khlaid samim <95288187+itkhld@users.noreply.github.com>
2023-04-28 01:26:31 +0300

    Create Question3.py

---
+++ Day 1 - Question 1 - 3/Question3.py
@@ -0,0 +1,22 @@
+"""
+                                                PYTHON 3
+THIRD EXAMPLE : With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an
+integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
+"""
+#FIRST SOLUTION
+n = int(input())
+ans = {}
+for i in range(1, 1+n):
+    ans[i]= i * i
+print(ans)
+
+
+#SECOND SOLUTION
+n = int(input())
+ans = {i: i*i for i in range(1, 1+n)}
+print(ans)
+
+
+#THIRD SOLUTION
+num = int(input())
+print(dict(enumerate([i * i for i in range(1, num+1)], 1)))
