88e4c48b37e3644bb6296513df45eb62c615fb39
khlaid samim <95288187+itkhld@users.noreply.github.com>
2023-04-28 01:26:56 +0300

    Update Question2.py

--- Day 1 - Question 1 - 3/Question2.py
+++ Day 1 - Question 1 - 3/Question2.py
@@ -4,7 +4,7 @@ SECOND EXAMPLE : Write a program which can compute the factorial of a given numb
 comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
 """
 #FIRST SOLUTION 
-n = int(input)
+n = int(input())
 
 fact = 1
 i = 1
@@ -14,7 +14,7 @@ print(fact)
 
 
 #SECOND SOLUTION
-n = int(input)
+n = int(input())
 def shortFact(x):
     return 1 if x <= 1 else x * shortFact(x-1)
 print(shortFact(n))
