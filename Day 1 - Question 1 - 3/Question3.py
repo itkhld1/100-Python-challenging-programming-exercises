d199818022ff832250a859d3c3e0d6310e5e0efc
Khalid Samim <126877550+itkhld1@users.noreply.github.com>
2023-04-29 14:13:46 +0300

    Update Question3.py

--- Day 1 - Question 1 - 3/Question3.py
+++ Day 1 - Question 1 - 3/Question3.py
@@ -1,6 +1,6 @@
 """
                                                 PYTHON 3
-THIRD EXAMPLE : With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an
+EXAMPLE THREE: With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an
 integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
 """
 #FIRST SOLUTION
