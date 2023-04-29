703e7e5bc340e70a14a3b3ec7004c7326b8880e1
Khalid Samim <126877550+itkhld1@users.noreply.github.com>
2023-04-29 14:14:08 +0300

    Update Question4.py

--- Day 2 - Question 4 - 9/Question4.py
+++ Day 2 - Question 4 - 9/Question4.py
@@ -1,6 +1,6 @@
 """
                                                 PYTHON 3
-FOURTH EXAMPLE : Write a program which accepts a sequence of comma-separated numbers from console and generate a
+EXAMPLE FOUR: Write a program which accepts a sequence of comma-separated numbers from console and generate a
 list and a tuple which contains every number.Suppose the following input is supplied to the program:
 """
 #FIRST SOLUTION 
