696661359e824bbb421764cc474af46218aa401c
khlaid samim <95288187+itkhld@users.noreply.github.com>
2023-05-01 20:28:36 +0300

    Create Question9.py

---
+++ Day 2 - Question 4 - 9/Question9.py
@@ -0,0 +1,35 @@
+"""
+                                                PYTHON 3
+EXAMPLE NINE: Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+
+Suppose the following input is supplied to the program:
+
+Hello world
+Practice makes perfect
+Then, the output should be:
+
+HELLO WORLD
+PRACTICE MAKES PERFECT
+"""
+#FIRST SOLUTION
+lst = []
+while True:
+    x = input()
+    if len(x) == 0:
+        break
+    lst.append(x.upper())
+
+for line in lst:
+    print(line)
+
+
+#SECOND SOLUTION
+def user_input():
+    while True:
+        s = input()
+        if not s:
+            return
+        yield s
+
+for line in map(str.upper, user_input()):
+    print(line)
